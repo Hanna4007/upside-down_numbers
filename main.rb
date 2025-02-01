de6a7349b7e2f3ff8787852edd4dcebef3e0e7fb
Anna Lobova <riabkova.as.87@gmail.com>
2025-02-01 21:32:05 +0200

    Added unnecessary_x, unnecessary_y

--- main.rb
+++ main.rb
@@ -1,15 +1,26 @@
 VALID_NUMBERS = ["0", "1", "6", "8", "9"]
 VALID_REVERSE_NUMBERS  = ["0", "1", "8"]
 
-def upside_down(x,y)  
-  arr_upside_down = []
-  (x.length..y.length).each do |length_of_number|
-    generate_arr_upside_down(length_of_number, x.to_i, y.to_i, arr_upside_down)
-  end
-  arr_upside_down.count
+VALID_NUMBERS_1 = ["9", "8", "6", "1", "0"]
+VALID_REVERSE_NUMBERS_1  = ["8", "1", "0"]
+
+def upside_down(x,y)
+  full_amount_of_upside_down_numbers(x, y) - unnecessary_x(x) - unnecessary_y(y)  
+end  
+
+def unnecessary_x(x)
+  arr_unnecessary_x = []  
+  generate_arr_unnecessary_x(x.length, x.to_i, arr_unnecessary_x)
+  arr_unnecessary_x.count
 end
 
-def generate_arr_upside_down(length_of_number, x, y, arr_upside_down)
+def unnecessary_y(y)
+  arr_unnecessary_y = []  
+  generate_arr_unnecessary_y(y.length, y.to_i, arr_unnecessary_y)
+  arr_unnecessary_y.count
+end
+
+def generate_arr_unnecessary_x(length_of_number, x, arr_unnecessary_x)
   half_length_of_number = length_of_number/2
   VALID_NUMBERS.repeated_permutation(half_length_of_number).each do |number|
     first_part = number.join("")
@@ -17,19 +28,51 @@ def generate_arr_upside_down(length_of_number, x, y, arr_upside_down)
     second_part = first_part.tr("69", "96").reverse
 
     if length_of_number.odd?
-      VALID_REVERSE_NUMBERS.each do |middle|        
-        add_num_if_valid(first_part + middle + second_part, x, y, arr_upside_down)
+      VALID_REVERSE_NUMBERS.each do |middle|
+        return unless add_to_unnecessary_x_if_valid(first_part + middle + second_part, x, arr_unnecessary_x)
       end
     else
-      add_num_if_valid(first_part + second_part, x, y, arr_upside_down)      
-    end    
+      return unless add_to_unnecessary_x_if_valid(first_part + second_part, x, arr_unnecessary_x)
+    end       
   end
 end
 
+def add_to_unnecessary_x_if_valid(generated_number, x, arr_unnecessary_x)
+  number = generated_number.to_i
+  if number >= x
+    nil
+  else
+    arr_unnecessary_x << number
+    number
+  end
+end
 
-def add_num_if_valid(num, x, y, arr_upside_down)
-  number = num.to_i
-  arr_upside_down << number if number.between?(x, y)
+def generate_arr_unnecessary_y(length_of_number, y, arr_unnecessary_y)
+  half_length_of_number = length_of_number/2
+  VALID_NUMBERS_1.repeated_permutation(half_length_of_number).each do |number|
+    first_part = number.join("")
+    next if first_part[0] == "0"
+    second_part = first_part.tr("69", "96").reverse
+
+    if length_of_number.odd?
+      VALID_REVERSE_NUMBERS_1.each do |middle|
+        return unless add_to_unnecessary_y_if_valid(first_part + middle + second_part, y, arr_unnecessary_y)
+      end
+    else
+      return unless add_to_unnecessary_y_if_valid(first_part + second_part, y, arr_unnecessary_y)
+    end       
+  end
+end
+
+def add_to_unnecessary_y_if_valid(generated_number, y, arr_unnecessary_y)
+  number = generated_number.to_i
+  if number <= y
+    nil
+  else
+    arr_unnecessary_y << number
+    number
+  end
+  
 end
 
 def full_amount_of_upside_down_numbers(x, y)
